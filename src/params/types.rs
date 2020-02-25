1755cbb5a7cc694a41b673dafd42267d49533a00
Moritz Borcherding <moritz.borcherding@web.de>
2020-02-25 19:43:09 +0100

    add a len implementation for containers

--- src/params/types.rs
+++ src/params/types.rs
@@ -161,3 +161,122 @@ pub struct Dict<'a, 'e: 'a> {
     pub value_sig: signature::Type,
     pub map: DictMap<'a, 'e>,
 }
+
+impl<'a, 'e> Param<'a, 'e> {
+    pub fn make_signature(&self, buf: &mut String) {
+        match self {
+            Param::Base(b) => b.make_signature(buf),
+            Param::Container(c) => c.make_signature(buf),
+        }
+    }
+    pub fn sig(&self) -> signature::Type {
+        match self {
+            Param::Base(b) => b.sig(),
+            Param::Container(c) => c.sig(),
+        }
+    }
+}
+
+impl<'a> Base<'a> {
+    pub fn make_signature(&self, buf: &mut String) {
+        match self {
+            Base::Boolean(_) => buf.push('b'),
+            Base::Double(_) => buf.push('d'),
+            Base::Byte(_) => buf.push('y'),
+            Base::Int16(_) => buf.push('n'),
+            Base::Uint16(_) => buf.push('q'),
+            Base::Int32(_) => buf.push('i'),
+            Base::Uint32(_) => buf.push('u'),
+            Base::UnixFd(_) => buf.push('h'),
+            Base::Int64(_) => buf.push('x'),
+            Base::Uint64(_) => buf.push('t'),
+            Base::ObjectPath(_) => buf.push('o'),
+            Base::String(_) => buf.push('s'),
+            Base::Signature(_) => buf.push('g'),
+            Base::BooleanRef(_) => buf.push('b'),
+            Base::DoubleRef(_) => buf.push('d'),
+            Base::ByteRef(_) => buf.push('y'),
+            Base::Int16Ref(_) => buf.push('n'),
+            Base::Uint16Ref(_) => buf.push('q'),
+            Base::Int32Ref(_) => buf.push('i'),
+            Base::Uint32Ref(_) => buf.push('u'),
+            Base::UnixFdRef(_) => buf.push('h'),
+            Base::Int64Ref(_) => buf.push('x'),
+            Base::Uint64Ref(_) => buf.push('t'),
+            Base::ObjectPathRef(_) => buf.push('o'),
+            Base::StringRef(_) => buf.push('s'),
+            Base::SignatureRef(_) => buf.push('g'),
+        }
+    }
+
+    pub fn sig(&self) -> signature::Type {
+        let sig: signature::Base = self.into();
+        signature::Type::Base(sig)
+    }
+}
+impl<'a, 'e> Container<'a, 'e> {
+    pub fn make_signature(&self, buf: &mut String) {
+        match self {
+            Container::Array(elements) => {
+                buf.push('a');
+                elements.element_sig.to_str(buf);
+            }
+            Container::Dict(map) => {
+                buf.push('a');
+                buf.push('{');
+                map.key_sig.to_str(buf);
+                map.value_sig.to_str(buf);
+                buf.push('}');
+            }
+            Container::Struct(elements) => {
+                buf.push('(');
+                for el in elements {
+                    el.make_signature(buf);
+                }
+                buf.push(')');
+            }
+            Container::Variant(_) => {
+                buf.push('v');
+            }
+            Container::ArrayRef(elements) => {
+                buf.push('a');
+                elements.element_sig.to_str(buf);
+            }
+            Container::DictRef(map) => {
+                buf.push('a');
+                buf.push('{');
+                map.key_sig.to_str(buf);
+                map.value_sig.to_str(buf);
+                buf.push('}');
+            }
+            Container::StructRef(elements) => {
+                buf.push('(');
+                for el in *elements {
+                    el.make_signature(buf);
+                }
+                buf.push(')');
+            }
+            Container::VariantRef(_) => {
+                buf.push('v');
+            }
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        match self {
+            Container::Array(elements) => elements.values.len(),
+            Container::ArrayRef(elements) => elements.values.len(),
+            Container::Dict(map) => map.map.len(),
+            Container::DictRef(map) => map.map.len(),
+            Container::Struct(elements) => elements.len(),
+            Container::StructRef(elements) => elements.len(),
+            Container::Variant(_) => 1,
+            Container::VariantRef(_) => 1,
+        }
+    }
+
+    pub fn sig(&self) -> signature::Type {
+        let sig: signature::Container = self.into();
+        signature::Type::Container(sig)
+    }
+}
