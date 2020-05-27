f97409458b8ef07c60451911b7f3117f2b382888
Moritz Borcherding <moritz.borcherding@web.de>
2020-05-27 14:11:50 +0200

    start a new API to build messages. The current way of creating the enums/structs is unergonomic and inefficient if the nesting gets deeper than one level

--- src/message_builder.rs
+++ src/message_builder.rs
@@ -98,3 +98,295 @@ impl<'a, 'e> SignalBuilder<'a, 'e> {
         self.msg
     }
 }
+
+pub struct OutMessageBody {
+    buf: Vec<u8>,
+    sig: String,
+}
+
+impl OutMessageBody {
+    pub fn new() -> Self {
+        OutMessageBody {
+            buf: Vec::new(),
+            sig: String::new(),
+        }
+    }
+
+    pub fn push_param<P: Marshal>(&mut self, p: P) -> Result<(), message::Error> {
+        p.signature().to_str(&mut self.sig);
+        p.marshal(message::ByteOrder::LittleEndian, &mut self.buf)
+    }
+}
+
+impl<E: Marshal> Marshal for &[E] {
+    fn marshal(
+        &self,
+        byteorder: message::ByteOrder,
+        buf: &mut Vec<u8>,
+    ) -> Result<(), message::Error> {
+        let size_pos = buf.len();
+        buf.push(0);
+        buf.push(0);
+        buf.push(0);
+        buf.push(0);
+
+        if self.len() > 0 && self[0].alignment() > 4 {
+            let pad_size = buf.len() % self[0].alignment();
+            eprintln!("pad_size: {}", pad_size);
+            for _ in 0..pad_size {
+                buf.push(0);
+            }
+        }
+
+        let size_before = buf.len();
+        for p in self.iter() {
+            p.marshal(byteorder, buf)?;
+        }
+        let size_of_content = buf.len() - size_before;
+        crate::wire::util::insert_u32(
+            byteorder,
+            size_of_content as u32,
+            &mut buf[size_pos..size_pos + 4],
+        );
+
+        Ok(())
+    }
+    fn signature(&self) -> crate::signature::Type {
+        crate::signature::Type::Container(crate::signature::Container::Array(Box::new(
+            self[0].signature(),
+        )))
+    }
+
+    fn alignment(&self) -> usize {
+        4
+    }
+}
+
+impl<K: Marshal, V: Marshal> Marshal for std::collections::HashMap<K, V> {
+    fn marshal(
+        &self,
+        byteorder: message::ByteOrder,
+        buf: &mut Vec<u8>,
+    ) -> Result<(), message::Error> {
+        let size_pos = buf.len();
+        buf.push(0);
+        buf.push(0);
+        buf.push(0);
+        buf.push(0);
+
+        if self.len() > 0 {
+            // always align to 8
+            let pad_size = buf.len() % 8;
+            eprintln!("pad_size: {}", pad_size);
+            for _ in 0..pad_size {
+                buf.push(0);
+            }
+        }
+
+        let size_before = buf.len();
+        for p in self.iter() {
+            // always align to 8
+            let pad_size = buf.len() % 8;
+            eprintln!("pad_size: {}", pad_size);
+            for _ in 0..pad_size {
+                buf.push(0);
+            }
+            p.0.marshal(byteorder, buf)?;
+            p.1.marshal(byteorder, buf)?;
+        }
+        let size_of_content = buf.len() - size_before;
+        crate::wire::util::insert_u32(
+            byteorder,
+            size_of_content as u32,
+            &mut buf[size_pos..size_pos + 4],
+        );
+
+        Ok(())
+    }
+
+    fn signature(&self) -> crate::signature::Type {
+        let ks = self.keys().nth(0).unwrap().signature();
+        let vs = self.values().nth(0).unwrap().signature();
+        if let crate::signature::Type::Base(ks) = ks {
+            crate::signature::Type::Container(crate::signature::Container::Dict(ks, Box::new(vs)))
+        } else {
+            panic!("Ivalid key sig")
+        }
+    }
+
+    fn alignment(&self) -> usize {
+        4
+    }
+}
+
+pub trait Marshal {
+    fn marshal(
+        &self,
+        byteorder: message::ByteOrder,
+        buf: &mut Vec<u8>,
+    ) -> Result<(), message::Error>;
+
+    fn signature(&self) -> crate::signature::Type;
+    fn alignment(&self) -> usize;
+}
+
+impl<'a> Marshal for params::Param<'a, 'a> {
+    fn marshal(
+        &self,
+        byteorder: message::ByteOrder,
+        buf: &mut Vec<u8>,
+    ) -> Result<(), message::Error> {
+        crate::wire::marshal_container::marshal_param(self, byteorder, buf)
+    }
+
+    fn signature(&self) -> crate::signature::Type {
+        self.sig()
+    }
+    fn alignment(&self) -> usize {
+        self.sig().get_alignment()
+    }
+}
+
+impl Marshal for u64 {
+    fn marshal(
+        &self,
+        byteorder: message::ByteOrder,
+        buf: &mut Vec<u8>,
+    ) -> Result<(), message::Error> {
+        let b: params::Base = self.into();
+        crate::wire::marshal_base::marshal_base_param(byteorder, &b, buf)
+    }
+
+    fn signature(&self) -> crate::signature::Type {
+        let b: params::Base = self.into();
+        b.sig()
+    }
+    fn alignment(&self) -> usize {
+        let b: params::Base = self.into();
+        b.sig().get_alignment()
+    }
+}
+
+impl Marshal for u32 {
+    fn marshal(
+        &self,
+        byteorder: message::ByteOrder,
+        buf: &mut Vec<u8>,
+    ) -> Result<(), message::Error> {
+        let b: params::Base = self.into();
+        crate::wire::marshal_base::marshal_base_param(byteorder, &b, buf)
+    }
+
+    fn signature(&self) -> crate::signature::Type {
+        let b: params::Base = self.into();
+        b.sig()
+    }
+    fn alignment(&self) -> usize {
+        let b: params::Base = self.into();
+        b.sig().get_alignment()
+    }
+}
+
+impl Marshal for u16 {
+    fn marshal(
+        &self,
+        byteorder: message::ByteOrder,
+        buf: &mut Vec<u8>,
+    ) -> Result<(), message::Error> {
+        let b: params::Base = self.into();
+        crate::wire::marshal_base::marshal_base_param(byteorder, &b, buf)
+    }
+
+    fn signature(&self) -> crate::signature::Type {
+        let b: params::Base = self.into();
+        b.sig()
+    }
+    fn alignment(&self) -> usize {
+        let b: params::Base = self.into();
+        b.sig().get_alignment()
+    }
+}
+
+impl Marshal for u8 {
+    fn marshal(
+        &self,
+        byteorder: message::ByteOrder,
+        buf: &mut Vec<u8>,
+    ) -> Result<(), message::Error> {
+        let b: params::Base = self.into();
+        crate::wire::marshal_base::marshal_base_param(byteorder, &b, buf)
+    }
+
+    fn signature(&self) -> crate::signature::Type {
+        let b: params::Base = self.into();
+        b.sig()
+    }
+    fn alignment(&self) -> usize {
+        let b: params::Base = self.into();
+        b.sig().get_alignment()
+    }
+}
+
+impl Marshal for dyn AsRef<str> {
+    fn marshal(
+        &self,
+        byteorder: message::ByteOrder,
+        buf: &mut Vec<u8>,
+    ) -> Result<(), message::Error> {
+        let b: params::Base = self.as_ref().into();
+        crate::wire::marshal_base::marshal_base_param(byteorder, &b, buf)
+    }
+
+    fn signature(&self) -> crate::signature::Type {
+        let b: params::Base = self.as_ref().into();
+        b.sig()
+    }
+    fn alignment(&self) -> usize {
+        let b: params::Base = self.as_ref().into();
+        b.sig().get_alignment()
+    }
+}
+
+impl Marshal for &str {
+    fn marshal(
+        &self,
+        byteorder: message::ByteOrder,
+        buf: &mut Vec<u8>,
+    ) -> Result<(), message::Error> {
+        let b: params::Base = (*self).into();
+        crate::wire::marshal_base::marshal_base_param(byteorder, &b, buf)
+    }
+
+    fn signature(&self) -> crate::signature::Type {
+        let b: params::Base = (*self).into();
+        b.sig()
+    }
+    fn alignment(&self) -> usize {
+        let b: params::Base = (*self).into();
+        b.sig().get_alignment()
+    }
+}
+
+#[test]
+fn test_marshal_trait() {
+    let mut body = OutMessageBody::new();
+    let bytes: &[&[_]] = &[&[4u64]];
+    body.push_param(bytes).unwrap();
+
+    assert_eq!(
+        vec![12, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],
+        body.buf
+    );
+    assert_eq!(body.sig.as_str(), "aat");
+
+    let mut body = OutMessageBody::new();
+    let mut map = std::collections::HashMap::new();
+    map.insert("a", 4u32);
+
+    body.push_param(map).unwrap();
+    assert_eq!(
+        vec![12, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, b'a', 0, 0, 0, 4, 0, 0, 0,],
+        body.buf
+    );
+    assert_eq!(body.sig.as_str(), "a{su}");
+}
