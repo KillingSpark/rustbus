e4cbe8b75a89d5315882db27a3bb17489a515402
Moritz Borcherding <moritz.borcherding@web.de>
2020-08-29 15:17:34 +0200

    add a macro that generates variants with given types and names, with fitting (un-)marshal impls

--- src/wire/mod.rs
+++ src/wire/mod.rs
@@ -149,3 +149,140 @@ fn variant_trait_impl() {
         )))
     );
 }
+
+#[test]
+fn test_variant_macro() {
+    macro_rules! dbus_variant {
+        ($vname: ident, $($name: ident => $typ: ident);+) => {
+            dbus_variant_type!($vname, $(
+                $name => $typ
+            )+);
+
+            impl marshal::traits::Signature for $vname {
+                fn signature() -> crate::signature::Type {
+                    crate::signature::Type::Container(crate::signature::Container::Variant)
+                }
+                fn alignment() -> usize {
+                    1
+                }
+            }
+
+            dbus_variant_marshal!($vname, $(
+                $name => $typ
+            )+);
+            dbus_variant_unmarshal!($vname, $(
+                $name => $typ
+            )+);
+        };
+    }
+
+    macro_rules! dbus_variant_type {
+        ($vname: ident, $($name: ident => $typ: ident)+) => {
+            #[derive(Eq, PartialEq, Debug)]
+            pub enum $vname {
+                $(
+                    $name($typ),
+                )+
+                Catchall(crate::signature::Type)
+            }
+        };
+    }
+
+    macro_rules! dbus_variant_marshal {
+        ($vname: ident, $($name: ident => $typ: ident)+) => {
+            impl crate::Marshal for $vname {
+                fn marshal(&self, byteorder: crate::ByteOrder, buf: &mut Vec<u8>) -> Result<(), crate::Error> {
+                    use crate::Signature;
+
+                    match self {
+                        $(
+                            Self::$name(v) => {
+                                let mut sig_str = String::new();
+                                $typ::signature().to_str(&mut sig_str);
+                                crate::wire::marshal::base::marshal_base_param(
+                                    byteorder,
+                                    &crate::params::Base::Signature(sig_str),
+                                    buf,
+                                )
+                                .unwrap();
+                                v.marshal(byteorder, buf)?;
+                            }
+                        )+
+                        Self::Catchall(_) => unimplemented!("Do not use Catchall for Marshal cases!"),
+                    }
+                    Ok(())
+                }
+            }
+        };
+    }
+    macro_rules! dbus_variant_unmarshal {
+        ($vname: ident, $($name: ident => $typ: ident)+) => {
+            impl<'ret, 'buf: 'ret> unmarshal::traits::Unmarshal<'ret, 'buf> for $vname {
+                fn unmarshal(
+                    byteorder: crate::ByteOrder,
+                    buf: &'buf [u8],
+                    offset: usize,
+                ) -> unmarshal::UnmarshalResult<Self> {
+                    use crate::Signature;
+
+                    let (bytes, sig_str) = crate::wire::util::unmarshal_signature(&buf[offset..])?;
+                    let mut sig = crate::signature::Type::parse_description(&sig_str)?;
+                    let sig = if sig.len() == 1 {
+                        sig.remove(0)
+                    } else {
+                        return Err(crate::wire::unmarshal::Error::WrongSignature);
+                    };
+                    let offset = offset + bytes;
+
+                    $(
+                    if sig == $typ::signature() {
+                        let (vbytes, v) = $typ::unmarshal(byteorder, buf, offset)?;
+                        return Ok((bytes + vbytes, Self::$name(v)));
+                    }
+                    )+
+                    let (vbytes, _p) = crate::wire::unmarshal::container::unmarshal_with_sig(
+                        byteorder, &sig, buf, offset,
+                    )?;
+                    Ok((bytes + vbytes, Self::Catchall(sig)))
+                }
+            }
+        };
+    }
+
+    use crate::Marshal;
+    use crate::Unmarshal;
+
+    let mut buf = vec![];
+    dbus_variant!(MyVariant, String => String; V2 => i32; Integer => u32);
+    let v1 = MyVariant::String("ABCD".to_owned());
+    let v2 = MyVariant::V2(0);
+    let v3 = MyVariant::Integer(100);
+
+    (&v1, &v2, &v3)
+        .marshal(crate::ByteOrder::LittleEndian, &mut buf)
+        .unwrap();
+    // add a unknown variant here
+    crate::message_builder::marshal_as_variant(0xFFFFu64, crate::ByteOrder::LittleEndian, &mut buf)
+        .unwrap();
+
+    let (bytes, (uv1, uv2, uv3)) = <(MyVariant, MyVariant, MyVariant) as Unmarshal>::unmarshal(
+        crate::ByteOrder::LittleEndian,
+        &buf,
+        0,
+    )
+    .unwrap();
+    assert_eq!(uv1, v1);
+    assert_ne!(uv1, v2);
+    assert_ne!(uv1, v3);
+
+    assert_eq!(uv2, v2);
+    assert_ne!(uv2, v3);
+
+    assert_eq!(uv3, v3);
+
+    let (_bytes, uv4) = MyVariant::unmarshal(crate::ByteOrder::LittleEndian, &buf, bytes).unwrap();
+    assert_eq!(
+        uv4,
+        MyVariant::Catchall(crate::signature::Type::Base(crate::signature::Base::Uint64))
+    );
+}
