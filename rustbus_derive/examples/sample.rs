c95769413bcd049858c5aa619d1c3d6f0c937d4b
Moritz Borcherding <moritz.borcherding@web.de>
2020-10-13 11:13:48 +0200

    introduce proc-macro crate that can derive Marshal and Unmarshal for structs

---
+++ rustbus_derive/examples/sample.rs
@@ -0,0 +1,71 @@
+use rustbus::MessageBuilder;
+use rustbus_derive::{Marshal, Signature, Unmarshal};
+
+#[derive(Marshal, Unmarshal, Signature, Default, Debug)]
+struct A {
+    y: u32,
+    x: u64,
+    strct: (u8, u8, String),
+    raw_data: Vec<u8>,
+    sub: SubTypeA,
+}
+
+#[derive(Marshal, Unmarshal, Signature, Default, Debug)]
+struct SubTypeA {
+    x: u8,
+    y: u16,
+    z: u32,
+    w: u64,
+    s: String,
+}
+
+#[derive(Marshal, Unmarshal, Signature, Default, Debug)]
+struct B<'a> {
+    y: u32,
+    x: u64,
+    strct: (u8, u8, &'a str),
+    raw_data: &'a [u8],
+    sub: SubTypeB<'a>,
+}
+
+#[derive(Marshal, Unmarshal, Signature, Default, Debug)]
+struct SubTypeB<'a> {
+    x: u8,
+    y: u16,
+    z: u32,
+    w: u64,
+    s: &'a str,
+}
+
+fn main() {
+    let a = A {
+        y: 0xAAAAAAAA,
+        x: 0xBBBBBBBBBBBBBBBB,
+        strct: (1, 2, "ABCD".into()),
+        raw_data: vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+        sub: SubTypeA {
+            x: 0,
+            y: 1,
+            z: 3,
+            w: 4,
+            s: "AA".into(),
+        },
+    };
+
+    // create a signal with the MessageBuilder API
+    let mut sig = MessageBuilder::new()
+        .signal(
+            "io.killing.spark".into(),
+            "TestSignal".into(),
+            "/io/killing/spark".into(),
+        )
+        .build();
+
+    // add a parameter to the signal
+    sig.body.push_param(&a).unwrap();
+
+    println!("{:#X?}", sig.body);
+
+    println!("{:#X?}", sig.body.parser().get::<A>().unwrap());
+    println!("{:#X?}", sig.body.parser().get::<B>().unwrap());
+}
---
+++ rustbus_derive/src/lib.rs
@@ -0,0 +1,177 @@
+use proc_macro2::TokenStream;
+use quote::{quote, ToTokens};
+
+#[proc_macro_derive(Marshal)]
+pub fn derive_marshal(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+
+    match ast.data {
+        syn::Data::Struct(data) => {
+            make_struct_marshal_impl(&ast.ident, &ast.generics, &data.fields).into()
+        }
+        _ => unimplemented!("Nothing but structs can be derived on right now"),
+    }
+}
+#[proc_macro_derive(Unmarshal)]
+pub fn derive_unmarshal(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+
+    match ast.data {
+        syn::Data::Struct(data) => {
+            make_struct_unmarshal_impl(&ast.ident, &ast.generics, &data.fields).into()
+        }
+        _ => unimplemented!("Nothing but structs can be derived on right now"),
+    }
+}
+#[proc_macro_derive(Signature)]
+pub fn derive_signature(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+
+    match ast.data {
+        syn::Data::Struct(data) => {
+            make_struct_signature_impl(&ast.ident, &ast.generics, &data.fields).into()
+        }
+        _ => unimplemented!("Nothing but structs can be derived on right now"),
+    }
+}
+
+fn make_struct_marshal_impl(
+    ident: &syn::Ident,
+    generics: &syn::Generics,
+    fields: &syn::Fields,
+) -> TokenStream {
+    let (impl_gen, typ_gen, clause_gen) = generics.split_for_impl();
+    let marshal = struct_field_marshal(fields);
+
+    quote! {
+        impl #impl_gen ::rustbus::Marshal for #ident #typ_gen #clause_gen {
+            #[inline]
+            fn marshal(&self, ctx: &mut ::rustbus::wire::marshal::MarshalContext<'_,'_>) -> Result<(), ::rustbus::Error> {
+                #marshal
+            }
+        }
+    }
+}
+fn make_struct_unmarshal_impl(
+    ident: &syn::Ident,
+    generics: &syn::Generics,
+    fields: &syn::Fields,
+) -> TokenStream {
+    let marshal = struct_field_unmarshal(fields);
+
+    let mut rdef = syn::LifetimeDef {
+        attrs: Vec::new(),
+        lifetime: syn::Lifetime::new("'__internal_r", proc_macro2::Span::call_site()),
+        colon_token: None,
+        bounds: syn::punctuated::Punctuated::new(),
+    };
+    let mut bufdef = syn::LifetimeDef {
+        attrs: Vec::new(),
+        lifetime: syn::Lifetime::new("'__internal_buf", proc_macro2::Span::call_site()),
+        colon_token: None,
+        bounds: syn::punctuated::Punctuated::new(),
+    };
+    bufdef.bounds.push(rdef.lifetime.clone());
+
+    let fdsdef = syn::LifetimeDef {
+        attrs: Vec::new(),
+        lifetime: syn::Lifetime::new("'__internal_fds", proc_macro2::Span::call_site()),
+        colon_token: None,
+        bounds: syn::punctuated::Punctuated::new(),
+    };
+
+    let mut new_generics = generics.clone();
+    new_generics
+        .lifetimes()
+        .for_each(|lt| rdef.bounds.push(lt.lifetime.clone()));
+        
+    let typ_generics = new_generics.clone();
+    let (_, typ_gen, _) = typ_generics.split_for_impl();
+
+    new_generics.params = vec![
+        syn::GenericParam::Lifetime(rdef),
+        syn::GenericParam::Lifetime(bufdef),
+        syn::GenericParam::Lifetime(fdsdef),
+    ]
+    .into_iter()
+    .chain(new_generics.params)
+    .collect();
+
+    let (impl_gen, _, clause_gen) = new_generics.split_for_impl();
+
+    quote! {
+        impl #impl_gen ::rustbus::Unmarshal<'__internal_r, '__internal_buf, '__internal_fds> for #ident #typ_gen #clause_gen {
+            #[inline]
+            fn unmarshal(ctx: &mut ::rustbus::wire::unmarshal::UnmarshalContext<'__internal_fds,'__internal_buf>) -> Result<(usize,Self), ::rustbus::wire::unmarshal::Error> {
+                #marshal
+            }
+        }
+    }
+}
+fn make_struct_signature_impl(
+    ident: &syn::Ident,
+    generics: &syn::Generics,
+    fields: &syn::Fields,
+) -> TokenStream {
+    let (impl_gen, typ_gen, clause_gen) = generics.split_for_impl();
+    let signature = struct_field_sigs(fields);
+
+    quote! {
+        impl #impl_gen ::rustbus::Signature for #ident #typ_gen #clause_gen {
+            #[inline]
+            fn signature() -> ::rustbus::signature::Type {
+                #signature
+            }
+            fn alignment() -> usize {
+                8
+            }
+        }
+    }
+}
+
+fn struct_field_marshal(fields: &syn::Fields) -> TokenStream {
+    let field_names = fields
+        .iter()
+        .map(|field| field.ident.as_ref().unwrap().to_token_stream());
+
+    quote! {
+            ctx.align_to(8);
+            #(
+                self.#field_names.marshal(ctx)?;
+            )*
+            Ok(())
+    }
+}
+fn struct_field_unmarshal(fields: &syn::Fields) -> TokenStream {
+    let field_names = fields
+        .iter()
+        .map(|field| field.ident.as_ref().unwrap().to_token_stream());
+
+    let field_types = fields.iter().map(|field| field.ty.to_token_stream());
+
+    quote! {
+            let start_offset = ctx.offset;
+            ctx.align_to(8)?;
+
+            let this = Self{
+                #(
+                    #field_names: <#field_types as ::rustbus::Unmarshal>::unmarshal(ctx)?.1,
+                )*
+            };
+            let total_bytes = ctx.offset - start_offset;
+            Ok((total_bytes, this))
+    }
+}
+fn struct_field_sigs(fields: &syn::Fields) -> TokenStream {
+    let field_types = fields.iter().map(|field| field.ty.to_token_stream());
+
+    quote! {
+            let mut sigs = vec![];
+
+            #(
+                sigs.push(<#field_types as rustbus::Signature>::signature());
+            )*
+
+            ::rustbus::signature::Type::Container(::rustbus::signature::Container::Struct(sigs))
+    }
+}
