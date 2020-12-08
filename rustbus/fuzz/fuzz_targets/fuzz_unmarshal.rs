10e79fcd2201ffbf851401dcdadc5defaf7355d2
Moritz Borcherding <moritz.borcherding@web.de>
2020-12-08 20:02:06 +0100

    improve fuzz target. This does hit an ICE currently but its good to have for the future

--- rustbus/fuzz/fuzz_targets/fuzz_unmarshal.rs
+++ rustbus/fuzz/fuzz_targets/fuzz_unmarshal.rs
@@ -1,7 +1,27 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
+#[macro_use]
+extern crate libfuzzer_sys;
 extern crate rustbus;
 
 fuzz_target!(|data: &[u8]| {
-    rustbus::wire::unmarshal::unmarshal_header(data, 0);
+    let (hdrbytes, header) = match rustbus::wire::unmarshal::unmarshal_header(data, 0) {
+        Ok(head) => head,
+        Err(_) => return,
+    };
+    let (dynhdrbytes, dynheader) =
+        match rustbus::wire::unmarshal::unmarshal_dynamic_header(&header, data, hdrbytes) {
+            Ok(head) => head,
+            Err(_) => return,
+        };
+
+    let (_bytes_used, msg) = match rustbus::wire::unmarshal::unmarshal_next_message(
+        &header,
+        dynheader,
+        data,
+        hdrbytes + dynhdrbytes,
+    ) {
+        Ok(msg) => msg,
+        Err(_) => return,
+    };
+    msg.unmarshall_all().ok();
 });
