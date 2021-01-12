b9372bb3c3ee053e8e46f7c25993856eb17e428c
Moritz Borcherding <moritz.borcherding@web.de>
2021-01-12 20:33:02 +0100

    add get2<_> calls to fuzzing

--- rustbus/fuzz/fuzz_targets/fuzz_unmarshal.rs
+++ rustbus/fuzz/fuzz_targets/fuzz_unmarshal.rs
@@ -47,6 +47,10 @@ fn try_unmarhal_traits(msg: &rustbus::message_builder::MarshalledMessage) {
     msg.body.parser().get::<i32>().ok();
     msg.body.parser().get::<i16>().ok();
     msg.body.parser().get::<u8>().ok();
+    msg.body
+        .parser()
+        .get::<rustbus::wire::unmarshal::traits::Variant>()
+        .ok();
 
     // some collections
     use std::collections::HashMap;
@@ -66,5 +70,57 @@ fn try_unmarhal_traits(msg: &rustbus::message_builder::MarshalledMessage) {
     msg.body
         .parser()
         .get::<Vec<HashMap<&str, Vec<(u8, u32, rustbus::wire::unmarshal::traits::Variant)>>>>()
+        .ok();
+
+    // Now do the same but with get2<_>
+    // any getX<_> essentially uses the same code so get2 should suffice
+
+    // base types
+    msg.body.parser().get2::<u8, &str>().ok();
+    msg.body
+        .parser()
+        .get2::<&str, rustbus::wire::marshal::traits::ObjectPath<&str>>()
+        .ok();
+    msg.body
+        .parser()
+        .get2::<&str, rustbus::wire::marshal::traits::SignatureWrapper>()
+        .ok();
+    msg.body.parser().get2::<&str, u64>().ok();
+    msg.body.parser().get2::<&str, u32>().ok();
+    msg.body.parser().get2::<&str, u16>().ok();
+    msg.body.parser().get2::<&str, i64>().ok();
+    msg.body.parser().get2::<&str, i32>().ok();
+    msg.body.parser().get2::<&str, i16>().ok();
+    msg.body.parser().get2::<&str, u8>().ok();
+    msg.body
+        .parser()
+        .get2::<&str, rustbus::wire::unmarshal::traits::Variant>()
+        .ok();
+
+    // some collections
+    msg.body
+        .parser()
+        .get2::<(u8, u64, u8, u32), (u8, &str, u8, u32)>()
+        .ok();
+    msg.body.parser().get2::<Vec<Vec<u8>>, Vec<u32>>().ok();
+    msg.body
+        .parser()
+        .get2::<HashMap<u32, i64>, Vec<(u8, u32)>>()
+        .ok();
+    msg.body
+        .parser()
+        .get2::<Vec<HashMap<&str, u8>>, HashMap<&str, (u8, u32)>>()
+        .ok();
+    msg.body
+        .parser()
+        .get2::<Vec<i32>, HashMap<&str, Vec<(u8, u32)>>>()
+        .ok();
+    msg.body
+        .parser()
+        .get2::<Vec<u32>, Vec<HashMap<&str, Vec<(u8, u32)>>>>()
+        .ok();
+    msg.body
+        .parser()
+        .get2::<Vec<(u8,u8,u32)>, Vec<HashMap<&str, Vec<(u8, u32, rustbus::wire::unmarshal::traits::Variant)>>>>()
         .ok();
 }
