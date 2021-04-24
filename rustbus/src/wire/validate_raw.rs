KillingSpark/rustbus

commit a43187b77afab6472ccd886089de9fc829fb4675

Optimized validate_marshalled_container

rustbus/src/wire/validate_raw.rs
@@ -150,14 +150,24 @@ pub fn validate_marshalled_container(
                 return Err((offset, Error::NotEnoughBytesForCollection));
             }
 
-            let mut bytes_used_counter = 0;
-            while bytes_used_counter < bytes_in_array as usize {
-                let bytes_used =
-                    validate_marshalled(byteorder, offset + bytes_used_counter, buf, &elem_sig)?;
-                bytes_used_counter += bytes_used;
+            let total_bytes_used = padding + 4 + first_elem_padding + bytes_in_array as usize;
+            if elem_sig.bytes_always_valid() {
+                if bytes_in_array as usize % elem_sig.get_alignment() != 0 {
+                    return Err((offset, Error::NotEnoughBytes));
+                }
+            } else {
+                let mut bytes_used_counter = 0;
+                let array_end = total_bytes_used + offset;
+                while bytes_used_counter < bytes_in_array as usize {
+                    let bytes_used = validate_marshalled(
+                        byteorder,
+                        offset + bytes_used_counter,
+                        &buf[..array_end],
+                        &elem_sig,
+                    )?;
+                    bytes_used_counter += bytes_used;
+                }
             }
-            let total_bytes_used = padding + 4 + first_elem_padding + bytes_used_counter;
-
             Ok(total_bytes_used)
         }
         signature::Container::Dict(key_sig, val_sig) => {

rustbus/src/signature.rs
@@ -192,7 +192,6 @@ impl Base {
             Base::Signature => buf.push('g'),
         }
     }
-
     pub fn get_alignment(self) -> usize {
         match self {
             Base::Boolean => 4,
@@ -208,6 +207,23 @@ impl Base {
             Base::String => 4,
             Base::ObjectPath => 4,
             Base::Signature => 1,
+        }
+    }
+    /// If every bit-pattern is valid for a type and
+    /// and the length of the type is a multiple of its
+    /// alignment then this will return true.
+    pub(crate) fn bytes_always_valid(&self) -> bool {
+        match self {
+            Base::Byte => true,
+            Base::Int16 => true,
+            Base::Uint16 => true,
+            Base::Int32 => true,
+            Base::Uint32 => true,
+            Base::Int64 => true,
+            Base::Uint64 => true,
+            Base::UnixFd => true,
+            Base::Double => true,
+            _ => false,
         }
     }
 }
@@ -268,7 +284,12 @@ impl Type {
             Type::Container(c) => c.get_alignment(),
         }
     }
-
+    pub(crate) fn bytes_always_valid(&self) -> bool {
+        match self {
+            Type::Base(b) => b.bytes_always_valid(),
+            Type::Container(_) => false,
+        }
+    }
     fn parse_next_type<I: Iterator<Item = Result<Token>>>(
         tokens: &mut I,
         delim: Option<Token>,

commit 4f882fcd9cf21a68d4458b342344fa97d656d7e9

Fix regression from a43187b7
Caused panic on valid arrays if they were the last element in
unmarshal buffer. A test case was added to check for this bug in the
future.

rustbus/src/wire/validate_raw.rs
@@ -150,7 +150,6 @@ pub fn validate_marshalled_container(
                 return Err((offset, Error::NotEnoughBytesForCollection));
             }
 
-            let total_bytes_used = padding + 4 + first_elem_padding + bytes_in_array as usize;
             if elem_sig.bytes_always_valid() {
                 // bytes_always_valid() only returns true for types whose
                 // length is equal to their alignment
@@ -160,7 +159,7 @@ pub fn validate_marshalled_container(
                 }
             } else {
                 let mut bytes_used_counter = 0;
-                let array_end = total_bytes_used + offset;
+                let array_end = offset + first_elem_padding + bytes_in_array as usize;
                 while bytes_used_counter < bytes_in_array as usize {
                     let bytes_used = validate_marshalled(
                         byteorder,
@@ -171,6 +170,7 @@ pub fn validate_marshalled_container(
                     bytes_used_counter += bytes_used;
                 }
             }
+            let total_bytes_used = padding + 4 + first_elem_padding + bytes_in_array as usize;
             Ok(total_bytes_used)
         }
         signature::Container::Dict(key_sig, val_sig) => {
@@ -323,6 +323,22 @@ fn test_raw_validation() {
         0,
         &valid_buf,
         &signature::Type::parse_description("(a(yubt)a{s(yx)})").unwrap()[0],
+    )
+    .unwrap();
+    fds.clear();
+    valid_buf.clear();
+
+    let mut ctx = MarshalContext {
+        buf: &mut valid_buf,
+        fds: &mut fds,
+        byteorder: ByteOrder::LittleEndian,
+    };
+    ["Hello, ", "World!"].marshal(&mut ctx).unwrap();
+    validate_marshalled(
+        ByteOrder::LittleEndian,
+        0,
+        &valid_buf,
+        &signature::Type::parse_description("as").unwrap()[0],
     )
     .unwrap();
 }